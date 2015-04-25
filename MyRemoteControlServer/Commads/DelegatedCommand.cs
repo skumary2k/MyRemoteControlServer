d5454d2429d181e814051135a991d7210a2fa529
skumary2k <skumary2k@gmail.com>
2015-04-25 20:10:28 +0530

    MVVM pattern implemented.

---
+++ MyRemoteControlServer/Commads/DelegatedCommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyRemoteControlServer.Commads
+{
+    public class DelegatedCommand
+    {
+    }
+}
